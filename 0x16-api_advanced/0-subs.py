2113eb8a1af3f11ea5c26a03fe8587663c89e77e
saadelhamidi <saadavicii123@gmail.com>
2024-08-13 11:55:42 -0700

    x

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,25 +1,19 @@
 #!/usr/bin/python3
-"""
-Contains the number_of_subscribers function
-"""
-
+""" returns the number of subscribers """
 import requests
 
+
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers for a given subreddit"""
-    if not subreddit or not isinstance(subreddit, str):
+    """ function that queries the Reddit API and
+    returns the number of subscribers """
+    if type(subreddit) is not str or subreddit is None:
         return 0
-    
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {'User-Agent': '0x16-api_advanced:project:v1.0.0 (by /u/firdaus_cartoon_jr)'}
-    
-    try:
-        response = requests.get(url, headers=headers)
-        response.raise_for_status()  # Raises an HTTPError if the response was unsuccessful
-        data = response.json()
-        subs = data.get("data", {}).get("subscribers", 0)
-        return subs
-    except (requests.RequestException, ValueError) as e:
-        # Log or handle the error as needed
-        print(f"Error: {e}")
+    apiurl = f"https://www.reddit.com/r/{subreddit}/about.json"
+    header = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        }
+    res = requests.get(apiurl, headers=header, allow_redirects=False)
+    if res.status_code == 404:
         return 0
+    num_subscribers = res.json().get("data").get("subscribers")
+    return num_subscribers
